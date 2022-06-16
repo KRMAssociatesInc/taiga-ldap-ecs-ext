f2d9609a0af7eb3c5f113a9e7649c91cb5e6ed63
Shane McClaughry <shane.mcclaughry@krminc.com>
2022-06-16 12:30:52 -0400

    [Post Taiga LDAP docker and configs to GitHub]
    Post Taiga LDAP docker and configs to GitHub

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# taiga-ldap-ecs-ext
-Necessary files for LDAP connection and hosting in AWS ECS.
+# Introduction
+The information provided stems from a need for Taiga to access AD account information only.  The following information is based off of the original Taiga containers and utilizes the same `docker-compose.yml` but with minimal modifications.  
+
+Images were created from Taiga version 6.5.0 and have been verified as working in tests.  Active directory connection was tested using Microsoft AD on an AWS environment.
+
+# Create Back Image
+To create the back image run the following from the base directory
+* `cd taiga-back`
+* `docker build --rm -t krminc/taiga-back .`
+
+# Create Front Image
+If testing as localhost, then the modification of the conf.json can be skipped
+
+Modify the following parameters and replace `localhost:9000` with users anticipated URL
+* `cd taiga-front`
+* `vi conf.json`
+* ```
+    "api": "http://localhost:9000/api/v1/"
+    "eventsUrl": "ws://localhost:9000/events"
+    ```
+
+To create the front image run the following from the base directory
+* `cd taiga-front`
+* `docker build --rm -t krminc/taiga-front .`
+
+# Update the Taiga Docker Compose
+To utilize these containers, you'll be replacing the following images in the standard Taiga `docker-compose.yml` file
+
+Replace `FROM taigaio/taiga-front:latest` with the following: `FROM krminc/taiga-back:latest`
+
+Once completed also replace the following `FROM taigaio/taiga-back:latest` with `FROM krminc/taiga-back:latest`
+
+The following parameters will need to be added to `taiga-back` environmental variables
+
+```    
+    ENABLE_LDAP: "True"
+    # LDAP over STARTTLS
+    LDAP_START_TLS: "False"
+    LDAP_SERVER: example.com
+    LDAP_PORT: 389
+    # LDAP bind and lookup properties
+    LDAP_BIND_DN: CN=ServiceAccount,OU=OrganizationalUnit,DC=example,DC=com
+    LDAP_SEARCH_BASE: OU=OrganizationalUnit,DC=example,DC=com
+    LDAP_BIND_PASSWORD: secure-password
+    LDAP_USERNAME_ATTRIBUTE: sAMAccountName
+    LDAP_EMAIL_ATTRIBUTE: mail
+    LDAP_FULL_NAME_ATTRIBUTE: cn
+```  
+
+The following will need to be added to the `taiga-front` environmental variables
+
+```    
+    TAIGA_LOGIN_FORM_TYPE: ldap
+``` 
+
+# Taking Taiga Docker to AWS ECS
+Included with the git repository is a [templated json file](taiga-ecs-json.json) that can be used to create a Taiga task inside of ECS.  This json template has been changed to mirror the examples provided by Taiga in the [30 minutes setup](https://resources.taiga.io/30min-setup/) guide.  While the template can be utilized to get a quick start in ECS with Taiga, it is still advised to follow the Taiga guide referenced and start with a test instance in Docker to understand the variables.
+
+# Host LDAP images in ECR
+The custom images included in this repository will need to be hosted in Elastic Container Register(ECR) prior to launching the Taiga task for the first time and updated in the json parameters.  To build and host these images please refer to [AWS ECS Reference Material](https://docs.aws.amazon.com/AmazonECR/latest/userguide/docker-push-ecr-image.html).
+
+# Verified working environment and reference material
+If this is your first time using AWS ECS, the follow [AWS ECS Reference Material](https://docs.aws.amazon.com/AmazonECS/latest/developerguide/create-service.html) can be used to create a service.  The task has been verified as working with a AWS t3.medium server, and can be increased or lowered as needed per environment by modifying the CPU and Memory limits in the task definition.
+
+When creating the task definition, the json template once changed to mirror the users environment can be used.  If the user would perfer to enter the values through the console by hand, the following reference material for [AWS Task Definition Reference Material](https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definitions.html).
+
+The task definition for Taiga utilizes the ECS bridge for network connection.  To fully utilize this feature, the systems will require linking.  These links are included in the task template provided and can be updated as needed per individual user environments.
---
+++ taiga-back/settings/config.py
@@ -0,0 +1,247 @@
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2021-present Kaleidos Ventures SL
+
+from .common import *
+from ldap3 import Tls
+import os, sys, ssl
+
+
+#########################################
+## GENERIC
+#########################################
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT','5432'),
+    }
+}
+SECRET_KEY = os.getenv('TAIGA_SECRET_KEY')
+
+TAIGA_SITES_SCHEME = os.getenv('TAIGA_SITES_SCHEME')
+TAIGA_SITES_DOMAIN = os.getenv('TAIGA_SITES_DOMAIN')
+FORCE_SCRIPT_NAME = os.getenv('TAIGA_SUBPATH', '')
+
+TAIGA_URL = f"{ TAIGA_SITES_SCHEME }://{ TAIGA_SITES_DOMAIN }{ FORCE_SCRIPT_NAME }"
+SITES = {
+        "api": { "name": "api", "scheme": TAIGA_SITES_SCHEME, "domain": TAIGA_SITES_DOMAIN },
+        "front": { "name": "front", "scheme": TAIGA_SITES_SCHEME, "domain": f"{ TAIGA_SITES_DOMAIN }{ FORCE_SCRIPT_NAME }" }
+}
+
+INSTANCE_TYPE = "D"
+
+WEBHOOKS_ENABLED = True
+
+# Setting DEFAULT_PROJECT_SLUG_PREFIX to false
+# removes the username from project slug
+DEFAULT_PROJECT_SLUG_PREFIX = os.getenv('DEFAULT_PROJECT_SLUG_PREFIX', 'False') == 'True'
+
+#########################################
+## MEDIA
+#########################################
+MEDIA_URL = f"{ TAIGA_URL }/media/"
+DEFAULT_FILE_STORAGE = "taiga_contrib_protected.storage.ProtectedFileSystemStorage"
+THUMBNAIL_DEFAULT_STORAGE = DEFAULT_FILE_STORAGE
+
+STATIC_URL = f"{ TAIGA_URL }/static/"
+
+
+#########################################
+## EMAIL
+#########################################
+# https://docs.djangoproject.com/en/3.1/topics/email/
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
+CHANGE_NOTIFICATIONS_MIN_INTERVAL = 120  # seconds
+
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'system@taiga.io')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'False') == 'True'
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False') == 'True'
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'localhost')
+EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'user')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'password')
+
+
+#########################################
+## SESSION
+#########################################
+SESSION_COOKIE_SECURE = os.getenv('SESSION_COOKIE_SECURE', 'True') == 'True'
+CSRF_COOKIE_SECURE = os.getenv('CSRF_COOKIE_SECURE', 'True') == 'True'
+
+
+#########################################
+## EVENTS
+#########################################
+EVENTS_PUSH_BACKEND = "taiga.events.backends.rabbitmq.EventsPushBackend"
+
+EVENTS_PUSH_BACKEND_URL = os.getenv('EVENTS_PUSH_BACKEND_URL')
+if not EVENTS_PUSH_BACKEND_URL:
+    EVENTS_PUSH_BACKEND_URL = f"amqp://{ os.getenv('RABBITMQ_USER') }:{ os.getenv('RABBITMQ_PASS') }@taiga-events-rabbitmq:5672/taiga"
+
+EVENTS_PUSH_BACKEND_OPTIONS = {
+    "url": EVENTS_PUSH_BACKEND_URL
+}
+
+
+#########################################
+## TAIGA ASYNC
+#########################################
+CELERY_ENABLED = os.getenv('CELERY_ENABLED', 'True') == 'True'
+from kombu import Queue  # noqa
+
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
+if not CELERY_BROKER_URL:
+    CELERY_BROKER_URL = f"amqp://{ os.getenv('RABBITMQ_USER') }:{ os.getenv('RABBITMQ_PASS') }@taiga-async-rabbitmq:5672/taiga"
+
+CELERY_RESULT_BACKEND = None # for a general installation, we don't need to store the results
+CELERY_ACCEPT_CONTENT = ['pickle', ]  # Values are 'pickle', 'json', 'msgpack' and 'yaml'
+CELERY_TASK_SERIALIZER = "pickle"
+CELERY_RESULT_SERIALIZER = "pickle"
+CELERY_TIMEZONE = 'Europe/Madrid'
+CELERY_TASK_DEFAULT_QUEUE = 'tasks'
+CELERY_QUEUES = (
+    Queue('tasks', routing_key='task.#'),
+    Queue('transient', routing_key='transient.#', delivery_mode=1)
+)
+CELERY_TASK_DEFAULT_EXCHANGE = 'tasks'
+CELERY_TASK_DEFAULT_EXCHANGE_TYPE = 'topic'
+CELERY_TASK_DEFAULT_ROUTING_KEY = 'task.default'
+
+
+#########################################
+##  REGISTRATION
+#########################################
+PUBLIC_REGISTER_ENABLED = os.getenv('PUBLIC_REGISTER_ENABLED', 'False') == 'True'
+
+
+#########################################
+## CONTRIBS
+#########################################
+
+# SLACK
+ENABLE_SLACK = os.getenv('ENABLE_SLACK', 'False') == 'True'
+if ENABLE_SLACK:
+    INSTALLED_APPS += [
+        "taiga_contrib_slack"
+    ]
+
+# GITHUB AUTH
+# WARNING: If PUBLIC_REGISTER_ENABLED == False, currently Taiga by default prevents the OAuth
+# buttons to appear for both login and register
+ENABLE_GITHUB_AUTH = os.getenv('ENABLE_GITHUB_AUTH', 'False') == 'True'
+if PUBLIC_REGISTER_ENABLED and ENABLE_GITHUB_AUTH:
+    INSTALLED_APPS += [
+        "taiga_contrib_github_auth"
+    ]
+    GITHUB_API_CLIENT_ID = os.getenv('GITHUB_API_CLIENT_ID')
+    GITHUB_API_CLIENT_SECRET = os.getenv('GITHUB_API_CLIENT_SECRET')
+
+# GITLAB AUTH
+# WARNING: If PUBLIC_REGISTER_ENABLED == False, currently Taiga by default prevents the OAuth
+# buttons to appear for both login and register
+ENABLE_GITLAB_AUTH = os.getenv('ENABLE_GITLAB_AUTH', 'False') == 'True'
+if PUBLIC_REGISTER_ENABLED and ENABLE_GITLAB_AUTH:
+    INSTALLED_APPS += [
+        "taiga_contrib_gitlab_auth"
+    ]
+    GITLAB_API_CLIENT_ID = os.getenv('GITLAB_API_CLIENT_ID')
+    GITLAB_API_CLIENT_SECRET = os.getenv('GITLAB_API_CLIENT_SECRET')
+    GITLAB_URL = os.getenv('GITLAB_URL')
+
+#########################################
+## LDAP
+#########################################
+
+if os.getenv('TAIGA_ENABLE_LDAP', os.getenv('ENABLE_LDAP', 'False')).lower() == 'true':
+    # see https://github.com/Monogramm/taiga-contrib-ldap-auth-ext
+    print("Taiga contrib LDAP Auth Ext enabled", file=sys.stderr)
+    INSTALLED_APPS += ["taiga_contrib_ldap_auth_ext"]
+
+    if os.getenv('TAIGA_LDAP_USE_TLS', os.getenv('LDAP_START_TLS', 'False')).lower() == 'true':
+        # Flag to enable LDAP with STARTTLS before bind
+        LDAP_START_TLS = True
+        LDAP_TLS_CERTS = Tls(validate=ssl.CERT_NONE, version=ssl.PROTOCOL_TLSv1, ciphers='RSA+3DES')
+    else:
+        LDAP_START_TLS = False
+
+    LDAP_SERVER = os.getenv('TAIGA_LDAP_SERVER', os.getenv('LDAP_SERVER'))
+    LDAP_PORT = int(os.getenv('TAIGA_LDAP_PORT', os.getenv('LDAP_PORT', '389')))
+
+    # Full DN of the service account use to connect to LDAP server and search for login user's account entry
+    # If LDAP_BIND_DN is not specified, or is blank, then an anonymous bind is attempated
+    LDAP_BIND_DN = os.getenv('TAIGA_LDAP_BIND_DN', os.getenv('LDAP_BIND_DN'))
+    LDAP_BIND_PASSWORD = os.getenv('TAIGA_LDAP_BIND_PASSWORD', os.getenv('LDAP_BIND_PASSWORD'))
+
+    # Starting point within LDAP structure to search for login user
+    # Something like 'ou=People,dc=company,dc=com'
+    LDAP_SEARCH_BASE = os.getenv('TAIGA_LDAP_BASE_DN', os.getenv('LDAP_SEARCH_BASE'))
+
+    # Additional search criteria to the filter (will be ANDed)
+    #LDAP_SEARCH_FILTER_ADDITIONAL = '(mail=*)'
+
+    # Names of attributes to get username, e-mail and full name values from
+    # These fields need to have a value in LDAP
+    LDAP_USERNAME_ATTRIBUTE = os.getenv('TAIGA_LDAP_USERNAME_ATTRIBUTE', os.getenv('LDAP_USERNAME_ATTRIBUTE'))
+    LDAP_EMAIL_ATTRIBUTE = os.getenv('TAIGA_LDAP_EMAIL_ATTRIBUTE', os.getenv('LDAP_EMAIL_ATTRIBUTE'))
+    LDAP_FULL_NAME_ATTRIBUTE = os.getenv('TAIGA_LDAP_FULL_NAME_ATTRIBUTE', os.getenv('LDAP_FULL_NAME_ATTRIBUTE'))
+
+    # Option to not store the passwords in the local db
+    if os.getenv('TAIGA_LDAP_SAVE_LOGIN_PASSWORD', os.getenv('LDAP_SAVE_LOGIN_PASSWORD', 'False')).lower() == 'false':
+        LDAP_SAVE_LOGIN_PASSWORD = False
+
+    # Fallback on normal authentication method if this LDAP auth fails. Uncomment to enable.
+    LDAP_FALLBACK = os.getenv('TAIGA_LDAP_FALLBACK', os.getenv('LDAP_FALLBACK', 'normal'))
+
+    # Function to map LDAP username to local DB user unique identifier.
+    # Upon successful LDAP bind, will override returned username attribute
+    # value. May result in unexpected failures if changed after the database
+    # has been populated.
+    def _ldap_slugify(uid: str) -> str:
+        # example: force lower-case
+        uid = uid.lower()
+        return uid
+
+    LDAP_MAP_USERNAME_TO_UID = _ldap_slugify
+
+#########################################
+## TELEMETRY
+#########################################
+ENABLE_TELEMETRY = os.getenv('ENABLE_TELEMETRY', 'True') == 'True'
+
+
+#########################################
+##  IMPORTERS
+#########################################
+ENABLE_GITHUB_IMPORTER = os.getenv('ENABLE_GITHUB_IMPORTER', 'False') == 'True'
+if ENABLE_GITHUB_IMPORTER:
+    IMPORTERS["github"] = {
+        "active": True,
+        "client_id": os.getenv('GITHUB_IMPORTER_CLIENT_ID'),
+        "client_secret": os.getenv('GITHUB_IMPORTER_CLIENT_SECRET')
+    }
+
+ENABLE_JIRA_IMPORTER = os.getenv('ENABLE_JIRA_IMPORTER', 'False') == 'True'
+if ENABLE_JIRA_IMPORTER:
+    IMPORTERS["jira"] = {
+        "active": True,
+        "consumer_key": os.getenv('JIRA_IMPORTER_CONSUMER_KEY'),
+        "cert": os.getenv('JIRA_IMPORTER_CERT'),
+        "pub_cert": os.getenv('JIRA_IMPORTER_PUB_CERT')
+    }
+
+ENABLE_TRELLO_IMPORTER = os.getenv('ENABLE_TRELLO_IMPORTER', 'False') == 'True'
+if ENABLE_TRELLO_IMPORTER:
+    IMPORTERS["trello"] = {
+        "active": True,
+        "api_key": os.getenv('TRELLO_IMPORTER_API_KEY'),
+        "secret_key": os.getenv('TRELLO_IMPORTER_SECRET_KEY')
+    }
